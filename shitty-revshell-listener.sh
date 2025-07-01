dae77b1537ac14537d63e54e8984fd181540d8ab
ruizii <santiago-rb1@outlook.es>
2025-07-01 18:13:31 -0500

    Remove unused logic and add ip alias

--- shitty-revshell-listener.sh
+++ shitty-revshell-listener.sh
@@ -11,23 +11,26 @@ if [[ "$#" -ne 2 ]]; then
 
     echo -e "\nUsage: shitty-revshell-listener.sh <OS> <PORT>"
     exit 1
+fi
+
+if uname -r | grep -q "microsoft"; then
+    copy='/mnt/c/Windows/System32/clip.exe'
+elif [[ "$XDG_SESSION_TYPE" == "x11" ]]; then
+    copy='xclip -selection clipboard'
 else
-    if uname -r | grep -q "microsoft"; then
-        copy='/mnt/c/Windows/System32/clip.exe'
-    elif [[ "$XDG_SESSION_TYPE" == "x11" ]]; then
-        copy='xclip -selection clipboard'
-    else
-        copy='wl-copy'
-    fi
-    if [[ "$1" == "linux" ]]; then
-        echo -n "stty rows 46 cols 190" | $copy
-        echo -e "\n${BLUE}[!]${ENDCOLOR} Fix stty size on the reverse shell"
-        echo -e "${BLUE}[!]${ENDCOLOR} ${YELLOW}stty rows 46 cols 190${ENDCOLOR} copied to clipboard\n"
-        stty raw -echo && rcat l -ie "/usr/bin/script -qc /bin/bash /dev/null; export TERM=xterm-256color; alias ls='ls --color=auto'; alias grep='grep --color=auto';" "$2" && reset
-    elif [[ "$1" == "windows" ]]; then
-        echo -ne "\n"
-        rlwrap nc -lvnp "$2"
-    else
-        echo -e "\n${RED}[x]${ENDCOLOR} Invalid option\n"
-    fi
+    copy='wl-copy'
+fi
+
+if [[ "$1" == "linux" ]]; then
+    echo -n "stty rows 46 cols 190" | $copy
+    echo -e "\n${BLUE}[!]${ENDCOLOR} Fix stty size on the reverse shell"
+    echo -e "${BLUE}[!]${ENDCOLOR} ${YELLOW}stty rows 46 cols 190${ENDCOLOR} copied to clipboard\n"
+
+    stty raw -echo && rcat listen -ie "/usr/bin/script -qc /bin/bash /dev/null; export TERM=xterm-256color; alias ls='ls --color=auto'; alias grep='grep --color=auto'; alias ip='ip --color=auto'" "$2" && reset
+elif [[ "$1" == "windows" ]]; then
+    echo -ne "\n"
+
+    rlwrap ncat -lvnp "$2"
+else
+    echo -e "\n${RED}[x]${ENDCOLOR} Invalid option\n"
 fi
